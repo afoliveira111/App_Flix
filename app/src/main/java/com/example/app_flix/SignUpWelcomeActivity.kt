1d66514940c3b35b54be133f6aa3c6c3b8c1ef6f
Felipe <felipetermometria@hotmail.com>
2023-12-15 17:50:05 -0300

    setSupportActionBar - menu

--- app/src/main/java/com/example/app_flix/SignUpWelcomeActivity.kt
+++ app/src/main/java/com/example/app_flix/SignUpWelcomeActivity.kt
@@ -2,10 +2,15 @@ package com.example.app_flix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class SignUpWelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_welcome)
+
+        val signUpToolbar: Toolbar = findViewById(R.id.signup_toolbar)
+        setSupportActionBar(signUpToolbar)
+
     }
 }
