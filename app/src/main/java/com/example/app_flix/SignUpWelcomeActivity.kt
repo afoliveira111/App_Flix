18b86e5c588f5b56e70c0c56d917bd6be02124e2
Felipe <felipetermometria@hotmail.com>
2023-12-15 16:12:48 -0300

    Orientation

---
+++ app/src/main/java/com/example/app_flix/SignUpWelcomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_flix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpWelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_welcome)
+    }
+}
